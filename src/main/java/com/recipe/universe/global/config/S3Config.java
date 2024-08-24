9b1b86f79df525f402fc7b9db65a9dea54db6fa4
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-08-24 10:40:51 +0900

    s3 configuration

---
+++ src/main/java/com/recipe/universe/global/config/S3Config.java
@@ -0,0 +1,42 @@
+package com.recipe.universe.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String s3BucketName; // use it in s3 service
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String privateKey;
+
+    @Bean
+    public AwsBasicCredentials awsBasicCredentials(){
+        return AwsBasicCredentials.create(
+                accessKey, privateKey
+        );
+    }
+
+    @Bean
+    public S3Client s3Client(){
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials()))
+                .build();
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+
+}
