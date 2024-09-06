8cbc22518918eaf1bb30ad2bf87f378bc0636f6f
donghyoya <dongwooya123@gmail.com>
2024-09-06 16:19:48 +0900

    test ci 50

--- backend/scripts/stop.sh
+++ backend/scripts/stop.sh
@@ -1,17 +1,4 @@
-# stop.sh
-echo "try catching 1"
-pwd
-ls -al
-sudo cd /home/ubuntu/recipeUniverse/backend/ # 정확한 경로로 이동
-if [ $? -ne 0 ]; then
-    echo "Failed to change directory."
-    exit 1
-fi
-pwd
-ls -al
+# stop.sh 수정
 echo "Stopping existing Docker containers..."
-sudo docker-compose down
-if [ $? -ne 0 ]; then
-    echo "Failed to stop Docker containers."
-    exit 1
-fi
+sudo docker-compose --project-directory /home/ubuntu/recipeUniverse/backend down
+
