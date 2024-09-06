ce94fbd41b3b21b7a36bc6ce7d008085ef36583d
donghyoya <dongwooya123@gmail.com>
2024-09-06 11:53:22 +0900

    test ci 37

--- backend/src/main/java/com/recipe/universe/UniverseApplication.java
+++ backend/src/main/java/com/recipe/universe/UniverseApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDa
 public class UniverseApplication {
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(UniverseApplication.class, args);
 	}
