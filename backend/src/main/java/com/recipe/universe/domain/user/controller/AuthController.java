8e4651f0a20edf18f4aa8bf85fc291ba88cf72a0
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-09-08 01:59:47 +0900

    JwtTokenService

--- backend/src/main/java/com/recipe/universe/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/recipe/universe/domain/user/controller/AuthController.java
@@ -1,22 +1,24 @@
 package com.recipe.universe.domain.user.controller;
 
 import com.recipe.universe.domain.user.controller.form.UserRegistForm;
+import com.recipe.universe.domain.user.jwt.dto.JwtTokenDto;
+import com.recipe.universe.domain.user.jwt.service.JwtTokenService;
 import com.recipe.universe.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
     private final UserService userService;
+    private JwtTokenService jwtTokenService;
 
     @PostMapping("/regist")
     public String regist(@RequestBody UserRegistForm form){
         Long save = userService.save(form.getId(), form.getPassword());
         return "Login Success";
     }
+
+
 }
