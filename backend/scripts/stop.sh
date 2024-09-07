341415aeab1e8558d2f7bb17ec027bc3be4618b0
donghyoya <dongwooya123@gmail.com>
2024-09-07 17:29:42 +0900

    test ci 55

--- backend/scripts/stop.sh
+++ backend/scripts/stop.sh
@@ -1,20 +1,15 @@
-# stop.sh 수정
-#!/bin/bash
-
-# 로그 파일 경로 설정
 LOG_FILE="/home/ubuntu/recipeUniverse/backend/deploy_stop.log"
 
-# 로그 파일 생성 및 기존 내용 삭제
-echo "Deployment Log - $(date)" > $LOG_FILE
+echo "123" >> $LOG_FILE
 
-# 로그에 출력값 기록
-echo "Stopping existing Docker containers..." | tee -a $LOG_FILE
+echo "Stopping existing Docker containers...TEST55" | tee -a $LOG_FILE
+
 ls -al | tee -a $LOG_FILE
 pwd | tee -a $LOG_FILE
-whoami | tree -a $LOG_FILE
+whoami | tee -a $LOG_FILE
 
 # 필요한 경로로 이동하여 다시 로그에 기록
-cd /home/ubuntu/recipeUniverse/backend  # docker-compose.yml이 있는 경로로 변경
+cd /home/ubuntu/recipeUniverse/backend
 ls -al | tee -a $LOG_FILE
 pwd | tee -a $LOG_FILE
 
