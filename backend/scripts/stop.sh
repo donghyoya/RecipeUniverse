fdb79a1adcaa252e2e06218fc4fcc6365cfcdd90
donghyoya <dongwooya123@gmail.com>
2024-09-09 15:06:10 +0900

    test ci 80

--- backend/scripts/stop.sh
+++ backend/scripts/stop.sh
@@ -1,18 +1,3 @@
-LOG_FILE="/home/ubuntu/recipeUniverse/backend/deploy_stop.log"
-
-echo $(pwd)
-
-echo "123" >> $LOG_FILE
-
-echo "Stopping existing Docker containers...TEST55" | tee -a $LOG_FILE
-
-ls -al | tee -a $LOG_FILE
-pwd | tee -a $LOG_FILE
-whoami | tee -a $LOG_FILE
-
-# 필요한 경로로 이동하여 다시 로그에 기록
-cd /home/ubuntu/recipeUniverse/backend
-ls -al | tee -a $LOG_FILE
-pwd | tee -a $LOG_FILE
-
+#!/bin/bash
+echo "Stopping Docker container..."
 sudo docker-compose down
