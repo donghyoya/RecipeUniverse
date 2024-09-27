59916c76cbfac1d2349739e6dbeab8ee7f88a4ca
donghyoya <dongwooya123@gmail.com>
2024-09-27 17:44:41 +0900

    add function like ingName in IngredientRepository

--- backend/src/main/java/com/recipe/universe/domain/ingredient/repository/IngredientRepository.java
+++ backend/src/main/java/com/recipe/universe/domain/ingredient/repository/IngredientRepository.java
@@ -3,8 +3,11 @@ package com.recipe.universe.domain.ingredient.repository;
 import com.recipe.universe.domain.ingredient.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     Optional<Ingredient> findByIngName(String ingName);
+    // like %ingName%
+    Optional<List<Ingredient>> findByIngNameContaining(String ingName);
 }
