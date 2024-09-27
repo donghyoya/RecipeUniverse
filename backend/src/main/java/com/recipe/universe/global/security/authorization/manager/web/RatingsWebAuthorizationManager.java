9ec10a8794d587a9b02efed13e1d4b5f30f17abf
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-09-28 00:32:01 +0900

    [feat] RatingsWebAuthorizationManager

--- backend/src/main/java/com/recipe/universe/global/security/authorization/manager/web/RatingsWebAuthorizationManager.java
+++ backend/src/main/java/com/recipe/universe/global/security/authorization/manager/web/RatingsWebAuthorizationManager.java
@@ -1,19 +1,32 @@
 package com.recipe.universe.global.security.authorization.manager.web;
 
+import com.recipe.universe.domain.dish.dish.entity.Dish;
+import com.recipe.universe.domain.rating.entity.UserDishRatings;
+import com.recipe.universe.domain.rating.repository.UserDishRatingsRepository;
 import com.recipe.universe.domain.rating.service.UserDishRatingsService;
+import com.recipe.universe.domain.user.role.entity.RoleName;
+import com.recipe.universe.global.security.authorization.manager.role.AuthorizationDelegator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class RatingsWebAuthorizationManager extends AbstractWebAuthorizationManager{
     private static final String SUPPORT_URI_PATTERN = "/ratings/**";
-    private final UserDishRatingsService ratingsService;
+    private final AuthorizationDelegator manager;
+    private final UserDishRatingsRepository ratingsRepository;
 
     @Override
     public String getPattern() {
@@ -22,7 +35,71 @@ public class RatingsWebAuthorizationManager extends AbstractWebAuthorizationMana
 
     @Override
     public AuthorizationDecision decide(Supplier<Authentication> authenticationSupplier, RequestAuthorizationContext context) {
-        return null;
+        HttpMethod method = getMethod(context);
+
+        // GET 명령은 다 승인
+        if(method == HttpMethod.GET){
+            return new AuthorizationDecision(true);
+        }
+
+        // 인증된 사용자인지 먼저 확인
+        AuthorizationDecision decision = manager.isAuthentication(authenticationSupplier);
+        if(!decision.isGranted()){
+            return decision;
+        }
+
+        // 금지된 사용자인가
+        decision = manager.hasRole(RoleName.ROLE_BANNED, authenticationSupplier);
+        if(decision.isGranted()){
+            return decision;
+        }
+
+        if(method == HttpMethod.POST){
+            return new AuthorizationDecision(true);
+        }
+
+        if(method == HttpMethod.PUT){
+            return put(authenticationSupplier, context);
+        }
+
+        if(method == HttpMethod.DELETE){
+            return delete(authenticationSupplier, context);
+        }
+
+
+        return new AuthorizationDecision(false);
     }
+
+    public Long extract(RequestAuthorizationContext context){
+        Map<String, String> map = extractPathVariable("/ratings/{id}", context);
+        String id = map.getOrDefault("id", "-1");
+        return Long.parseLong(id);
+    }
+
+    private AuthorizationDecision delete(Supplier<Authentication> authenticationSupplier, RequestAuthorizationContext context){
+        Long id = extract(context);
+        if(id == -1l){
+            return new AuthorizationDecision(false);
+        }
+        return new AuthorizationDecision(check(id, Long.valueOf(authenticationSupplier.get().getName())));
+    }
+
+    private AuthorizationDecision put(Supplier<Authentication> authenticationSupplier, RequestAuthorizationContext context){
+        Long id = extract(context);
+        if(id == -1l){
+            return new AuthorizationDecision(false);
+        }
+        return new AuthorizationDecision(check(id, Long.valueOf(authenticationSupplier.get().getName())));
+
+    }
+
+    @Transactional(readOnly = true)
+    protected boolean check(Long id, Long userId){
+        Optional<UserDishRatings> ratings = ratingsRepository.findById(id);
+        return ratings.map(
+                value -> value.getUserId().equals(userId)
+        ).orElse(false);
+    }
+
 
 }
