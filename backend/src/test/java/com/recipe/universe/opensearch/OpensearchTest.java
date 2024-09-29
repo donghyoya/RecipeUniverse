70888cb9d7803c95393cea90ae4887caa022410d
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-09-29 18:46:36 +0900

    [config] fix config for opensearch

---
+++ backend/src/test/java/com/recipe/universe/opensearch/OpensearchTest.java
@@ -0,0 +1,41 @@
+package com.recipe.universe.opensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class OpensearchTest {
+    @Autowired private ElasticsearchOperations operations;
+
+    @Test
+    public void createIndex() {
+        // 인덱스가 존재하지 않으면 생성
+        boolean indexExists = operations.indexOps(IndexCoordinates.of("dummy")).exists();
+        if (!indexExists) {
+            operations.indexOps(Dummy.class).create();
+            operations.indexOps(Dummy.class).putMapping();
+        }
+    }
+
+    @Document(indexName = "dummy")
+    public static class Dummy {
+
+        @Id
+        private String id;
+        private String name;
+        private boolean isDelete;
+
+        public Dummy(String id, String name, boolean isDelete) {
+            this.id = id;
+            this.name = name;
+            this.isDelete = isDelete;
+        }
+    }
+}
