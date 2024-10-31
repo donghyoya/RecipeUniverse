fe1529ed1a49dacc07c62b273e6d381ceac003f4
donghyoya <dongwooya123@gmail.com>
2024-10-31 13:30:15 +0900

    chatgpt chatting setting

---
+++ backend/src/main/java/com/recipe/universe/domain/chatgpt/service/ChatGptService.java
@@ -0,0 +1,53 @@
+package com.recipe.universe.domain.chatgpt.service;
+
+import com.recipe.universe.domain.chatgpt.dto.ChatGptRequestDto;
+import com.recipe.universe.domain.chatgpt.dto.ChatGptResponseDto;
+import com.recipe.universe.domain.chatgpt.dto.QuestionRequestDto;
+import com.recipe.universe.global.config.ChatGptConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatGptService {
+
+    private final ChatGptConfig chatGptConfig;
+
+
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    public HttpEntity<ChatGptRequestDto> buildHttpEntity(ChatGptRequestDto requestDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE));
+        headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + chatGptConfig.getAPI_KEY());
+        return new HttpEntity<>(requestDto, headers);
+    }
+
+    public ChatGptResponseDto getResponse(HttpEntity<ChatGptRequestDto> chatGptRequestDtoHttpEntity) {
+        ResponseEntity<ChatGptResponseDto> responseEntity = restTemplate.postForEntity(
+                ChatGptConfig.URL,
+                chatGptRequestDtoHttpEntity,
+                ChatGptResponseDto.class);
+
+        return responseEntity.getBody();
+    }
+
+    public ChatGptResponseDto askQuestion(QuestionRequestDto requestDto) {
+        return this.getResponse(
+                this.buildHttpEntity(
+                        new ChatGptRequestDto(
+                                chatGptConfig.getMODEL(),
+                                requestDto.getQuestion(),
+                                ChatGptConfig.MAX_TOKEN,
+                                ChatGptConfig.TEMPERATURE,
+                                ChatGptConfig.TOP_P
+                        )
+                )
+        );
+    }
+}
