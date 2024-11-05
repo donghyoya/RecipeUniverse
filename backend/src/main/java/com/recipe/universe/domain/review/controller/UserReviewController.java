e619ed179611035331db161ab8778514d5ba108f
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-11-05 22:27:27 +0900

    docs: review controller

--- backend/src/main/java/com/recipe/universe/domain/review/controller/UserReviewController.java
+++ backend/src/main/java/com/recipe/universe/domain/review/controller/UserReviewController.java
@@ -5,12 +5,15 @@ import com.recipe.universe.domain.like.service.UserLikeService;
 import com.recipe.universe.domain.review.controller.form.UserReviewForm;
 import com.recipe.universe.domain.review.dto.UserReviewDto;
 import com.recipe.universe.domain.review.service.UserReviewService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "리뷰 API")
 @RequestMapping("/review")
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +21,7 @@ public class UserReviewController {
     private final UserReviewService reviewService;
     private final UserLikeService userLikeService;
 
+    @Operation(summary = "리뷰 작성 API")
     @SecurityRequirement(name = "JWT")
     @PostMapping()
     public UserReviewDto createReview(@RequestBody UserReviewForm form, Authentication authentication){
@@ -29,6 +33,7 @@ public class UserReviewController {
         return reviewService.findById(ratingId);
     }
 
+    @Operation(summary = "리뷰 삭제")
     @SecurityRequirement(name = "JWT")
     @PostMapping("/{id}/delete")
     public ResponseEntity<Void> updateReview(@PathVariable("id") Long id) {
@@ -36,6 +41,7 @@ public class UserReviewController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "리뷰 수정")
     @SecurityRequirement(name = "JWT")
     @PostMapping("/{id}/update")
     public UserReviewDto updateReview(@PathVariable("id") Long id, @RequestBody UserReviewForm form){
@@ -43,6 +49,7 @@ public class UserReviewController {
     }
 
     /* 좋아요 */
+    @Operation(summary = "리뷰 좋아요하기")
     @SecurityRequirement(name = "JWT")
     @PostMapping("/{id}/like")
     public UserLikeDto likeRecipe(@PathVariable("id") Long id, Authentication authentication) {
@@ -50,6 +57,7 @@ public class UserReviewController {
         return userLikeService.toggleReviewUser(userId, id);
     }
 
+    @Operation(summary = "리뷰 좋아요 여부 및 개수확인")
     @GetMapping("/{id}/like")
     public UserLikeDto getReviewLike(@PathVariable("id") Long id, Authentication authentication){
         Long userId = 0l;
--- backend/src/main/java/com/recipe/universe/domain/review/controller/form/UserReviewForm.java
+++ backend/src/main/java/com/recipe/universe/domain/review/controller/form/UserReviewForm.java
@@ -1,10 +1,14 @@
 package com.recipe.universe.domain.review.controller.form;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UserReviewForm {
+    @Schema(description = "리뷰를 작성할 레시피의 id")
     private Long recipeId;
+    @Schema(description = "리뷰 rating")
     private Double rating;
+    @Schema(description = "리뷰 내용")
     private String review;
 }
